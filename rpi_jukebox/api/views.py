9a259466fecfd573a4aca6d7740f74a64dee9f3f
ImamAzim <imam.usmani@sfr.fr>
2021-08-04 15:52:54 +0200

    basic template

--- rpi_jukebox/api/views.py
+++ rpi_jukebox/api/views.py
@@ -1,8 +1,10 @@
 import signal
 import sys
 from functools import partial
+import os
+import pickle
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Resource, Api
 
 from rpi_jukebox import resources
@@ -13,15 +15,35 @@ def main():
 
 def initiate_api():
 
+    LOCAL_DIRECTORY = os.path.join(os.path.dirname(__file__), '..', 'db')
+    DB_FILENAME = os.path.join(LOCAL_DIRECTORY, 'musics.pickle')
+
 
     app = Flask(__name__)
     app.config.from_object('rpi_jukebox.api.config')
     # To get one variable, tape app.config['MY_VARIABLE']
     api = Api(app)
 
-    api.add_resource(resources.homepage.Homepage, '/')
     api.add_resource(resources.jukebox.Jukebox, '/jukebox')
     api.add_resource(resources.jukebox.Music, '/jukebox/<rfid>')
+
+    @app.route('/')
+    def home_page():
+        try:
+            with open(DB_FILENAME, 'rb') as myfile:
+                musics = pickle.load(myfile)
+        except EOFError:
+            print('last parameter file is probably empty..')
+            musics = dict()
+        except IOError:
+            print('could not access or find last parameter file')
+            musics = dict()
+        lines = list()
+        for rfid, title in musics.items():
+            lines.append("<p> {}: {}</p>".format(rfid, title))
+        html_content = os.linesep.join(lines)
+        return render_template('index.html', name='imam')
+        # return html_content
 
     signal.signal(signal.SIGTERM, partial(stop_server))
     signal.signal(signal.SIGINT, partial(stop_server))
--- rpi_jukebox/resources/homepage.py
+++ rpi_jukebox/resources/homepage.py
@@ -1,5 +1,24 @@
+import os
+import pickle
+
 from flask_restful import Resource
+
+LOCAL_DIRECTORY = os.path.join(os.path.dirname(__file__), '..', 'db')
+DB_FILENAME = os.path.join(LOCAL_DIRECTORY, 'musics.pickle')
 
 class Homepage(Resource):
     def get(self):
-        return 'homepage'
+        try:
+            with open(DB_FILENAME, 'rb') as myfile:
+                musics = pickle.load(myfile)
+        except EOFError:
+            print('last parameter file is probably empty..')
+            musics = dict()
+        except IOError:
+            print('could not access or find last parameter file')
+            musics = dict()
+        lines = list()
+        for rfid, title in musics.items():
+            lines.append("<p> {}: {}</p>".format(rfid, title))
+        html_content = os.linesep.join(lines)
+        return html_content
