8d598c561dd849b5df19e296430bed0e6775f207
ImamAzim <imam.usmani@sfr.fr>
2021-08-04 14:53:03 +0200

    post

--- rpi_jukebox/resources/jukebox.py
+++ rpi_jukebox/resources/jukebox.py
@@ -1,13 +1,15 @@
 import os
 import pickle
 
-from flask_restful import Resource
+from flask import request
+from flask_restful import Resource, abort
 
 def main():
     print(musics)
 
 LOCAL_DIRECTORY = os.path.join(os.path.dirname(__file__), '..', 'db')
 DB_FILENAME = os.path.join(LOCAL_DIRECTORY, 'musics.pickle')
+print(DB_FILENAME)
 
 try:
     with open(DB_FILENAME, 'rb') as myfile:
@@ -19,15 +21,25 @@ except IOError:
     print('could not access or find last parameter file')
     musics = dict()
 
-# try:
-    # with open(DB_FILENAME, 'wb') as myfile:
-        # pickle.dump(musics, myfile)
-# except IOError:
-    # print('could not access or find last parameter file')
 
 class Jukebox(Resource):
+
     def get(self):
         return musics
+
+    def post(self):
+        rfid = request.form['rfid']
+        if rfid not in musics.keys():
+            musics[rfid] = None
+            try:
+                with open(DB_FILENAME, 'wb') as myfile:
+                    pickle.dump(musics, myfile)
+            except IOError:
+                print('could not access or find last parameter file')
+            return rfid, 201
+        else:
+            abort(422, message='rfid {} is already in the database'.format(rfid))
+
 
 if __name__=='__main__':
     main()
