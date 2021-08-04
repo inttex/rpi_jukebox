2a4e7ec482d8f6445e690ba19c095ae44ba14f4e
ImamAzim <imam.usmani@sfr.fr>
2021-08-04 16:39:11 +0200

    post on homepage

--- rpi_jukebox/resources/homepage.py
+++ rpi_jukebox/resources/homepage.py
@@ -4,5 +4,6 @@ def index(musics):
         lines = list()
         for rfid, title in musics.items():
             lines.append("<p> {}: {}</p>".format(rfid, title))
+        lines.append("<form method=post action=/jukebox> <input type='text' name='rfid'/><input type='submit' value='post'/></form>")
         html_content = os.linesep.join(lines)
         return html_content
