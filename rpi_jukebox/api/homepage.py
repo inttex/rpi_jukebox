0d999f701c8dec5243d4768a6794b721f90ef330
imamus <imam.usmani@alpeslasers.ch>
2021-08-06 13:44:33 +0200

    musics with 2 keys

--- rpi_jukebox/api/homepage.py
+++ rpi_jukebox/api/homepage.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, render_template, url_for
 
 def main():
-    musics = {'123': 'titre1', '123': 'titre2'}
+    musics = {'123': 'titre1', '124': 'titre2'}
     html_content = index(musics)
 
     app = Flask(__name__)
