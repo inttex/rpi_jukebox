eeb1eb5a98e57f0618919c99a45101c126db08ab
imamus <imam.usmani@alpeslasers.ch>
2021-08-06 13:26:38 +0200

    test mode of homepage

--- rpi_jukebox/api/homepage.py
+++ rpi_jukebox/api/homepage.py
@@ -1,9 +1,26 @@
 import os
 
+from flask import Flask, render_template, url_for
+
+def main():
+    musics = {'123': 'titre1', '123': 'titre2'}
+    html_content = index(musics)
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home_page():
+        return html_content
+    app.run(debug=True)
+
 def index(musics):
         lines = list()
         for rfid, title in musics.items():
-            lines.append("<p> {}: {}</p>".format(rfid, title))
+            html = "<p> {}: {}</p>".format(rfid, title)
+            lines.append(html)
         lines.append("<form method=post action=/jukebox> <input type='text' name='rfid'/><input type='submit' value='post'/></form>")
         html_content = os.linesep.join(lines)
         return html_content
+
+if __name__=='__main__':
+    main()
