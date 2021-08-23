523759fc9cdb1cc54d359b3af01c74754c83b5c3
ImamAzim <imam.usmani@sfr.fr>
2021-08-23 14:50:06 +0200

    rfid in db

--- rpi_jukebox/api/models.py
+++ rpi_jukebox/api/models.py
@@ -12,9 +12,10 @@ class Musics(Base):
     __tablename__ = 'musics'
     id = Column(Integer, primary_key=True)
     title = Column(String(80), unique=True, nullable=False)
+    rfid = Column(Integer, unique=True, nullable=False)
 
     def __repr__(self):
-        return '<Musics %r>' % self.title
+        return '<User %r>' % (self.title)
 
 if __name__=='__main__':
     db.create_all()
