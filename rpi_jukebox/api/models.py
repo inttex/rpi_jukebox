dce0b28462f388c2ade8fae611ffa3da92b6a983
ImamAzim <imam.usmani@sfr.fr>
2021-08-23 11:50:47 +0200

    model

---
+++ rpi_jukebox/api/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from rpi_jukebox.api.views import app
+
+db = SQLAlchemy(app)
+
+class Musics(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Musics %r>' % self.title
