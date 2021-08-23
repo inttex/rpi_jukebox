dbcbc9a15860cd03e011aeaf7f29b0f146f1118c
ImamAzim <imam.usmani@sfr.fr>
2021-08-23 13:22:11 +0200

    models in homepage

--- rpi_jukebox/api/homepage.py
+++ rpi_jukebox/api/homepage.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask, render_template, url_for
 
+from rpi_jukebox.api import models
+
 def main():
     musics = {'123': 'titre1', '124': 'titre2'}
     html_content = index(musics)
