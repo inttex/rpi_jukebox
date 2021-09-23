0d7175fa6d16e730664ef4c14a003fd4e4ac66f1
imamus <imam.usmani@alpeslasers.ch>
2021-09-23 13:56:03 +0200

    run loop

--- rpi_jukebox/client/player.py
+++ rpi_jukebox/client/player.py
@@ -2,12 +2,24 @@ import datetime
 import warnings
 import sys
 import os
+import requests
 
 import keyboard
 import simpleaudio
 
+HOST = 'http://localhost:5000'
+
 def main():
-    run()
+    resource = HOST + '/jukebox'
+    rfid = 4
+    rsp = requests.get(resource + '/{}'.format(rfid))
+    status = rsp.status_code
+    if status == 404:
+        print('no resource, create a new one')
+    elif status == 200:
+        print('play music')
+    else:
+        print('no music, do nothing')
 
 def run():
     play_obj = False
@@ -19,12 +31,28 @@ def run():
         now = datetime.datetime.now()
         msg = 'detected rfid no {} at {}\n'.format(rfid, now)
         print(msg)
+        resource = HOST + '/jukebox'
+        rsp = requests.get(resource + '/{}'.format(rfid))
+        status = rsp.status_code
+        if status == 404:
+            create_new_resource(rfid)
+        elif status == 200:
+            play_obj = play_music(rsp, play_obj)
+        else:
+            print('no music, do nothing')
+
+def create_new_resource(rfid):
+    print('no resource, create a new one')
+
+def play_music(rsp, play_obj):
+    if play_obj:
+        play_obj.stop()
+    print('play music')
             # msg = 'play {}\n'.format(music)
             # print(msg)
-        if play_obj:
-            play_obj.stop()
-        # wave_obj = simpleaudio.WaveObject.from_wave_file(os.path.join(MUSIC_DIRECTORY, music))
-        # play_obj =wave_obj.play()
+    # wave_obj = simpleaudio.WaveObject.from_wave_file(os.path.join(MUSIC_DIRECTORY, music))
+    # play_obj =wave_obj.play()
+    return play_obj
 
 if __name__=='__main__':
     main()
