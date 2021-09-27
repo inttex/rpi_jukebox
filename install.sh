c40b07636f95b6f8042224810618f1a8de82e542
ImamAzim <imam.usmani@sfr.fr>
2021-09-27 09:41:23 +0200

    create virtual env

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 if [ $UID = 0 ]
 then
-	echo
+	mkdir -p ~/.virtualenvs
+	python3 -m venv ~/.virtualenvs/rpi_jukebox
 else
 	echo "please run this script as root. create one if necessary with the command:
 	sudo passwd root"
 fi
-#-create virtual env python3
 #-activate env
 #-pip install .
 #-create .local/rpi_jukebox
