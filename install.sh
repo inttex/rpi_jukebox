4d4df7c23dded710c48c741dd90d2b702513951e
ImamAzim <imam.usmani@sfr.fr>
2021-09-27 09:39:28 +0200

    check if root

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
--create virtual env python3
--activate env
--pip install .
--create .local/rpi_jukebox
--create db
+if [ $UID = 0 ]
+then
+	echo
+else
+	echo "please run this script as root. create one if necessary with the command:
+	sudo passwd root"
+fi
+#-create virtual env python3
+#-activate env
+#-pip install .
+#-create .local/rpi_jukebox
+#-create db
