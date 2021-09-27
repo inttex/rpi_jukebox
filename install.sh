306c41968f31c0c632c49ed4d4c55faa91b33c9a
ImamAzim <imam.usmani@sfr.fr>
2021-09-27 09:42:03 +0200

    activ env

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ if [ $UID = 0 ]
 then
 	mkdir -p ~/.virtualenvs
 	python3 -m venv ~/.virtualenvs/rpi_jukebox
+	source ~/.virtualenvs/rpi_jukebox/bin/activate
 else
 	echo "please run this script as root. create one if necessary with the command:
 	sudo passwd root"
 fi
-#-activate env
 #-pip install .
 #-create .local/rpi_jukebox
 #-create db
