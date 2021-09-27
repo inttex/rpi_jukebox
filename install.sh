df1874bc648e3a32edd449acd02ea0b58337f13c
ImamAzim <imam.usmani@sfr.fr>
2021-09-27 09:49:22 +0200

    pip install

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ then
 	mkdir -p ~/.virtualenvs
 	python3 -m venv ~/.virtualenvs/rpi_jukebox
 	source ~/.virtualenvs/rpi_jukebox/bin/activate
+	pip install `dirname $0`
 else
 	echo "please run this script as root. create one if necessary with the command:
 	sudo passwd root"
 fi
-#-pip install .
 #-create .local/rpi_jukebox
 #-create db
