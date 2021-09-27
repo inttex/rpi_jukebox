ac31ac8e863e1c108e8c620cfb519d046ba1f62a
ImamAzim <imam.usmani@sfr.fr>
2021-09-27 09:54:15 +0200

    local dir

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ then
 	python3 -m venv ~/.virtualenvs/rpi_jukebox
 	source ~/.virtualenvs/rpi_jukebox/bin/activate
 	pip install `dirname $0`
+	mkdir -p $HOME/.local/share/rpi_jukebox
 else
 	echo "please run this script as root. create one if necessary with the command:
 	sudo passwd root"
 fi
-#-create .local/rpi_jukebox
 #-create db
