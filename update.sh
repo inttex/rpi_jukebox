5df857be2141c07fb03e17f76953ddf41109a885
ImamAzim <imam@e.email>
2022-07-11 17:12:29 +0200

    decativate env after update

--- update.sh
+++ update.sh
@@ -11,7 +11,7 @@ then
 		git -C `dirname $0` pull
 		source ~/.virtualenvs/rpi_jukebox/bin/activate
 		pip install `dirname $0`
-		echo "done! please restart"
+		deactivate
 	else
 		echo "please run this script as root. create one if necessary with the command:
 		sudo passwd root"
