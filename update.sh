898ab29aadd995d0a8db519516943acbca1a3e3b
ImamAzim <imam@e.email>
2022-07-11 17:08:35 +0200

    check if updates

--- update.sh
+++ update.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-if [ $UID = 0 ]
+
+path=`dirname $0`
+remote_commit=`git -C $path rev-parse origin/main`
+local_commit=`git -C $path rev-parse main`
+
+if [ $remote_commit != $local_commit ]
 then
-	git -C `dirname $0` pull
-	source ~/.virtualenvs/rpi_jukebox/bin/activate
-	pip install `dirname $0`
-	echo "done! please restart"
+	if [ $UID = 0 ]
+	then
+		git -C `dirname $0` pull
+		source ~/.virtualenvs/rpi_jukebox/bin/activate
+		pip install `dirname $0`
+		echo "done! please restart"
+	else
+		echo "please run this script as root. create one if necessary with the command:
+		sudo passwd root"
+	fi
 else
-	echo "please run this script as root. create one if necessary with the command:
-	sudo passwd root"
+	echo "no new updates available"
 fi
