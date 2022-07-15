b8e120c5dc1aee2b1c5d67c7f242988b7a7f74b0
ImamAzim <imam@e.email>
2022-07-15 17:02:03 +0200

    load last parameters

--- rpi_jukebox/api/config.py
+++ rpi_jukebox/api/config.py
@@ -2,14 +2,16 @@ import os
 
 import xdg
 
+from rpi_jukebox.utils import tools
 
 DATA_PATH = os.path.join(xdg.xdg_data_home(), 'rpi_jukebox')
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DATA_PATH, 'musics.db')
 
-LAST_PARAMETERS_FILE = os.path.join(DATA_PATH, 'last_parameter.json')
+LAST_PARAMETERS_FILE = os.path.join(DATA_PATH, 'last_parameters.json')
 
 default_parameters = dict(
         random_stop=False,
         )
 
+parameters = tools.load_last_parameters(default_parameters, LAST_PARAMETERS_FILE)
--- rpi_jukebox/utils/tools.py
+++ rpi_jukebox/utils/tools.py
@@ -9,7 +9,7 @@ def load_last_parameters(default_parameters: dict, path: str):
 
     parameters = dict(default_parameters)
     try:
-        with open(LAST_PARAMETERS_FILE, 'r') as myfile:
+        with open(path, 'r') as myfile:
             last_parameters = json.load(myfile)
         for key, el in last_parameters.items():
             if key in parameters:
