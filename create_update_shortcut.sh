f986d4e7e26d3dabb59751b423056d63a8d575b2
Imam Usmani <imam.usmani@alpeslasers.ch>
2022-07-20 21:09:19 +0200

    update log

--- create_update_shortcut.sh
+++ create_update_shortcut.sh
@@ -2,5 +2,5 @@
 
 path=`dirname $0`
 echo "#!/bin/bash" > ~/bin/update_rpi_jukebox
-echo `realpath $path/update.sh` >> ~/bin/update_rpi_jukebox
+echo `realpath $path/update.sh > /root/.local/share/rpi_jukebox/last_update_log 2>&1` >> ~/bin/update_rpi_jukebox
 chmod +x ~/bin/update_rpi_jukebox
