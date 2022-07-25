663c597c7025a7ae0d2244e2b2daf2488318c18c
ImamAzim <imam@e.email>
2022-07-25 18:17:34 +0200

    jukeboxview

---
+++ rpi_jukebox/client/views.py
@@ -0,0 +1,16 @@
+class JukeboxView():
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def wait_for_input(self):
+        pass
+
+    def play(self, sound):
+        pass
+
+    def stop(self):
+        pass
