70e6c1ca299bc680ec836c70881e577e696788ba
ImamAzim <imam@e.email>
2022-07-25 18:06:42 +0200

    apicommunicator

---
+++ rpi_jukebox/client/models.py
@@ -0,0 +1,45 @@
+def main():
+    pass
+
+
+class APICommunicator(object):
+
+    """communicate by requests to the api restless server"""
+
+    def __init__(self):
+        """TODO: to be defined. """
+
+    def get_music_file(self, rfid):
+        """obtain wav file from server
+
+        :rfid: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def get_random_stop(self):
+        """TODO: Docstring for get_random_stop.
+
+        :returns: TODO
+
+        """
+        pass
+
+    def get_tmin(self):
+        """TODO: Docstring for get_tmin.
+        :returns: TODO
+
+        """
+        pass
+
+    def get_tmax(self):
+        """get max time for random_stop
+        :returns: TODO
+
+        """
+        pass
+
+
+if __name__=='__main__':
+    main()
