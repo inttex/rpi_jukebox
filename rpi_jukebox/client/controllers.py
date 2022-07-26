0dad2fa9e613b033c9c3e4ee6385f657416768bb
imam <imam@e.email>
2022-07-26 16:14:06 +0200

    process_rfid

--- rpi_jukebox/client/controllers.py
+++ rpi_jukebox/client/controllers.py
@@ -5,7 +5,7 @@ def main():
     musicloader = MusicLoader()
     view = JukeboxView()
     controller = JukeboxController(apicommunicator, musicloader, view)
-    print(dir(controller))
+    controller.process_rfid(1)
 
 
 class JukeboxController():
@@ -13,6 +13,25 @@ class JukeboxController():
         self.apicommunicator = apicommunicator
         self.musicloader = musicloader
         self.view = view
+
+    def process_rfid(self, rfid):
+        if rfid is not None:
+            self.view.stop()
+            if not rfid == self.musicloader.rfid:
+                wav_file, success = self.apicommunicator.get_music_file(rfid)
+            else:
+                wav_file = None
+                success = True
+            if success:
+                if wav_file:
+                    random_stop = self.apicommunicator.get_random_stop()
+                    self.musicloader.random_stop = random_stop
+                    if random_stop:
+                        self.musicloader.tmin = self.apicommunicator.get_tmin()
+                        self.musicloader.tmax = self.apicommunicator.get_tmax()
+                sound = self.musicloader.get_sound(wav_file)
+                self.view.play(sound)
+                self.musicloader.rfid = rfid
 
 
 if __name__ == '__main__':
