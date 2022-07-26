fb24c8ec2e27dca03d5ea7cf3a9e02ec93794d73
imam <imam@e.email>
2022-07-26 14:57:10 +0200

    init controller

--- rpi_jukebox/client/controllers.py
+++ rpi_jukebox/client/controllers.py
@@ -1,9 +1,18 @@
 def main():
-    controller = JukeboxController()
+    from rpi_jukebox.client.models import APICommunicator, MusicLoader
+    from rpi_jukebox.client.views import JukeboxView
+    apicommunicator = APICommunicator()
+    musicloader = MusicLoader()
+    view = JukeboxView()
+    controller = JukeboxController(apicommunicator, musicloader, view)
+    print(dir(controller))
 
 
 class JukeboxController():
-    pass
+    def __init__(self, apicommunicator, musicloader, view):
+        self.apicommunicator = apicommunicator
+        self.musicloader = musicloader
+        self.view = view
 
 
 if __name__ == '__main__':
