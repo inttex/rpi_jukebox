87b89fae59ec23da82cee166bde3fa94bde2d9cf
imam <imam@e.email>
2022-07-28 13:53:53 +0200

    url dic in apicomm class

--- rpi_jukebox/client/models.py
+++ rpi_jukebox/client/models.py
@@ -1,19 +1,29 @@
 def main():
     HOST = 'http://localhost:5000'
     api_communicator = APICommunicator(HOST)
-    rfid = 1
-    wav_file, success = api_communicator.get_music_file(rfid)
-    print(wav_file)
-    print(success)
+    api_communicator2 = APICommunicator('abc')
+    print(api_communicator.url)
+    print(api_communicator2.url)
+    # rfid = 1
+    # wav_file, success = api_communicator.get_music_file(rfid)
+    # print(wav_file)
+    # print(success)
 
 
 class APICommunicator(object):
 
     """communicate by requests to the api restless server"""
+    entry_points = dict(
+            jukebox='/jukebox',
+            random_stop='/parameters/random_stop',
+            tmin='parameters/tmin',
+            tmax='parameters/tmax',
+            )
 
     def __init__(self, host):
         """TODO: to be defined. """
         self.host = host
+        self.url ={name: self.host + '/' + entry for name, entry in self.entry_points.items()}
 
     def get_music_file(self, rfid):
         """obtain wav file from server
@@ -22,6 +32,35 @@ class APICommunicator(object):
         :returns: wav_file, success
 
         """
+        url = f'{self.url['jukebox']}/{rfid}'
+
+        # rsp = requests.get(resource + '/{}'.format(rfid))
+        # status = rsp.status_code
+        # if status == 404:
+            # create_new_resource(rfid)
+            # logging.info('create new entry point for this rfid')
+        # elif status == 200:
+            # if not rfid==previous_rfid:
+                # simpleaudio.stop_all()
+                # play_obj = play_music(rsp)
+            # else:
+                # if play_obj.is_playing():
+                    # play_obj.pause()
+                    # play_obj.resume()
+                # else:
+                    # play_obj = play_music(rsp)
+            # url = HOST + '/parameters/random_stop'
+            # rsp = requests.get(url)
+            # random_stop = rsp.json()
+            # if random_stop:
+                # TIME = random.randint(TMIN, TMAX)
+                # random_stopper = threading.Timer(TIME, play_obj.pause)
+                # logging.info('the music of play object no %s will be paused in %s s', play_obj.play_id, TIME)
+                # random_stopper.start()
+            # previous_rfid = rfid
+        # else:
+            # logging.info('did not get a music, do nothing')
+
         wav_file = None
         success = False
         return wav_file, success
