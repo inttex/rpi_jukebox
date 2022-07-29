e843067b8fc27e1bfab804f24c6b94e5af4311e8
ImamAzim <imam@e.email>
2022-07-29 16:41:34 +0200

    create update shortcut in usr/local/bin

--- create_update_shortcut.sh
+++ create_update_shortcut.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 path=`dirname $0`
-echo "#!/bin/bash" > ~/bin/update_rpi_jukebox
-echo "`realpath $path/update.sh` > /root/.local/share/rpi_jukebox/last_update_log 2>&1" >> ~/bin/update_rpi_jukebox
-chmod +x ~/bin/update_rpi_jukebox
+echo "#!/bin/bash" > /usr/local/bin/update_rpi_jukebox
+echo "`realpath $path/update.sh` > /root/.local/share/rpi_jukebox/last_update_log 2>&1" >> /usr/local/bin/update_rpi_jukebox
+chmod +x /usr/local/bin/update_rpi_jukebox
